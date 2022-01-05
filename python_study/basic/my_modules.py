374c38ac008c85c81d15b165f76c6bbe2c2581c7
sara <deokku04@gmail.com>
2022-01-05 23:57:23 +0900

    2022-01-05 23:56 python study directory modify

--- python_study/basic/my_modules.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from pandas import Series, DataFrame
-from datetime import datetime
-from pandas.tseries.offsets import Day, Hour, Second
-from datetime import timedelta
-import matplotlib.pyplot as plt
-from numpy import nan as NA
-import math
